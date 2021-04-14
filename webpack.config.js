3c12a45674f60e93a8a8cfb13fa89158e5b7314b
dudtjr913 <64782636+dudtjr913@users.noreply.github.com>
2021-04-14 11:40:00 +0900

    feat: babel/plugin-transform-runtime추가

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,15 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['react-hot-loader/babel'],
+            plugins: [
+              'react-hot-loader/babel',
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  corejs: 3,
+                },
+              ],
+            ],
           },
         },
       },
